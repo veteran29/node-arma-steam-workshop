edba6f5c4c1170057e2ed4720713d1907757e745
Filip Maciejewski <veteran29q@gmail.com>
2019-03-05 22:51:58 +0100

    Detect that mods were not fully installed
    eg. SteamCMD download timeout

--- src/mods.js
+++ src/mods.js
@@ -24,7 +24,7 @@ function getModsFromACF (acf) {
     mods[id] = details[id]
   }
 
-  return mods
+  return { installed: installed, all: mods }
 }
 
 module.exports = function (steamDirectory, currentDownloads, callback) {
@@ -34,7 +34,7 @@ module.exports = function (steamDirectory, currentDownloads, callback) {
     }
 
     var acfMods = getModsFromACF(acf)
-    var ids = Object.keys(acfMods)
+    var ids = Object.keys(acfMods.all)
 
     Object.keys(currentDownloads).forEach(function (id) {
       if (ids.indexOf(id) === -1) {
@@ -57,13 +57,15 @@ module.exports = function (steamDirectory, currentDownloads, callback) {
         var folder = path.join(steamDirectory, 'steamapps', 'workshop', 'content', '107410', id)
         var title = mod.title
 
-        var acfMod = acfMods[id]
+        var acfMod = acfMods.all[id]
         var partial = false
         var outdated = false
+        var installed = false
 
         if (acfMod) {
           partial = acfMod.BytesToDownload > 0
           outdated = mod.time_updated > acfMod.timeupdated
+          installed = !!acfMods.installed[id]
         }
 
         var needsUpdate = downloading || outdated || partial
@@ -73,7 +75,8 @@ module.exports = function (steamDirectory, currentDownloads, callback) {
           id: id,
           name: title || id,
           needsUpdate: needsUpdate,
-          path: folder
+          path: folder,
+          installed: installed
         }
       }).filter(function (mod) {
         return mod && mod.id && mod.name && mod.path
